99f352501e120361ad6ba2573f468a9f4e366ac1
jaylco <77386599+jaylco@users.noreply.github.com>
2022-08-11 14:32:01 +0100

    Added test factory to run tests in JAR

--- src/main/java/uk/gov/cslearning/acceptanceTests/AcceptanceTestsApplication.java
+++ src/main/java/uk/gov/cslearning/acceptanceTests/AcceptanceTestsApplication.java
@@ -1,15 +1,52 @@
 package uk.gov.cslearning.acceptanceTests;
 
-import org.springframework.boot.SpringApplication;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import org.junit.platform.launcher.listeners.TestExecutionSummary;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPackage;
 
 @SpringBootApplication
 @SpringBootTest
 public class AcceptanceTestsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AcceptanceTestsApplication.class, args);
+
+		final LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+				.selectors(
+						selectPackage("test")
+				)
+				.build();
+
+		final Launcher launcher = LauncherFactory.create();
+
+		final boolean pathContainsTests = launcher.discover(request).containsTests();
+		if (!pathContainsTests) {
+			System.out.println("This path is invalid or folder doesn't consist tests");
+		}
+
+		final SummaryGeneratingListener listener = new SummaryGeneratingListener();
+
+		launcher.execute(request, listener);
+
+		final TestExecutionSummary summary = listener.getSummary();
+
+		final long containersFoundCount = summary.getContainersFoundCount();
+		System.out.println("containers Found Count  " + containersFoundCount);
+
+		final long containersSkippedCount = summary.getContainersSkippedCount();
+		System.out.println("containers Skipped Count  " + containersSkippedCount);
+
+		final long testsFoundCount = summary.getTestsFoundCount();
+		System.out.println("tests Found Count  " + testsFoundCount);
+
+		final long testsSkippedCount = summary.getTestsSkippedCount();
+		System.out.println("tests Skipped Count  " + testsSkippedCount);
 	}
 
 }
